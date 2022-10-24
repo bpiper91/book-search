ae4d716d0108316383e71a2c8f36f0740cafa94e
bpiper91 <bpiper91@gmail.com>
2022-10-23 23:41:06 -0400

    updated server logic to work with apollo, added typeDefs

--- server/server.js
+++ server/server.js
@@ -1,11 +1,21 @@
 const express = require('express');
+const { ApolloServer } = require('apollo-server-express');
 const path = require('path');
+
+const { typeDefs, resolvers } = require('schemas');
+const { authMiddleware } = require('./utils/auth'); // needs updating
 const db = require('./config/connection');
-const routes = require('./routes');
+
+// const routes = require('./routes');
+
+const PORT = process.env.PORT || 3001;
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  context: authMiddleware
+});
 
 const app = express();
-const PORT = process.env.PORT || 3001;
-
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
@@ -14,8 +24,14 @@ if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
 }
 
-app.use(routes);
+// app.use(routes);
 
-db.once('open', () => {
-  app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
-});
+const startApolloServer = async (typeDefs, resolvers) => {
+  await server.start();
+  server.applyMiddleware({ app });
+
+  db.once('open', () => {
+    app.listen(PORT, () => console.log(`🌍 Now listening on port ${PORT}`));
+    console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
+  })
+};
